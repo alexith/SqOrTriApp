5967618db320c0184add70aaaed250d486e07d30
alexith <akshay.cy2@gmail.com>
2020-05-14 13:59:33 +0530

    init commit of new app called sqOrTriApp

---
+++ app/src/main/java/com/example/sqortriapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.sqortriapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void showMeTheTruth(View view){
+
+        EditText enteredNumber = (EditText) findViewById(R.id.enteredNumber);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

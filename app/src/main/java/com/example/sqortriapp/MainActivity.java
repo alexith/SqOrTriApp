3e220267773969141764aa0a3fd3f6d6475d4857
alexith <akshay.cy2@gmail.com>
2020-05-15 17:59:23 +0530

    init commit of new app called sqOrTriApp

--- app/src/main/java/com/example/sqortriapp/MainActivity.java
+++ app/src/main/java/com/example/sqortriapp/MainActivity.java
@@ -13,49 +13,45 @@ public class MainActivity<integer> extends AppCompatActivity {
 
         final EditText[] enteredNumber = {findViewById(R.id.enteredNumber)};
 
-         class someNumbers{
+        class someNumbers{
+            private int i = 1;
+            private int n = 1;
+            private int currentTriNumber = 1;
+            private int currentSqNumber = 1;
 
-             private int i = 1;
-             private int n = 1;
-             private int currentTriNumber = 1;
-             private int currentSqNumber = 1;
-
-
-             private someNumbers<integer>[] squareNumberList = new someNumbers[0];
-             private someNumbers<integer>[] triangularNumbersList = new someNumbers[0];
+            private someNumbers<integer>[] triangularNumbersList = new someNumbers[0];
+            private someNumbers<integer>[] squareNumberList = new someNumbers[0];
 
 
-             for(n<=100){
-                 currentSqNumber = n*n;
-                 squareNumberList.add(currentSqNumber);
-                 n++;
-             }
+            for(n<=100){
+                currentTriNumber = currentTriNumber + i;
+                triangularNumbersList.add(currentTriNumber);
+                n++;
+            }
 
-
-             for(n<=100){
-                 currentTriNumber = currentTriNumber + i;
-                 triangularNumbersList.add(currentTriNumber);
-                 n++;
-             }
+            for(n<=100){
+                currentSqNumber = n*n;
+                squareNumberList.add(currentSqNumber);
+                n++;
+            }
 
              public boolean isNumberSq() {
-                 if(squareNumberList.contains(enteredNumber){
-                     Toast.makeText(MainActivity.this, "THe no. is Square", Toast.LENGTH_SHORT).show();
-                 }
-                 } else {
-                     Toast.makeText(MainActivity.this, "The no. is not Square", Toast.LENGTH_SHORT).show();
-             }
+                if(squareNumberList.contains(enteredNumber){
+                    Toast.makeText(MainActivity.this, "THe no. is Square", Toast.LENGTH_SHORT).show();
+                }
+                } else{
+                    Toast.makeText(MainActivity.this, "The no. is not Square", Toast.LENGTH_SHORT).show();
+            }
+             public boolean isTheNumberTri() {
+                if((triangularNumbersList.contains(enteredNumber)){
+                    Toast.makeText(MainActivity.this, "THe no. is Triangular", Toast.LENGTH_SHORT).show();
+                }
+            } else{
+                Toast.makeText(MainActivity.this, "The no. is not Triangular", Toast.LENGTH_SHORT).show();
+            }
 
-             public boolean isTheNumberTri(){
-                 if((triangularNumbersList.contains(enteredNumber)){
-                     return true;
-                 }else{
-                     return false;
-                 }
-             }
-         }
+        }
     }
-
 
 
     @Override

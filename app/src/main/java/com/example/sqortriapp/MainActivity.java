4f180775d12e2db1724dee492e46780b97e97aa3
alexith <akshay.cy2@gmail.com>
2020-05-15 07:46:33 +0530

    init commit of new app called sqOrTriApp

--- app/src/main/java/com/example/sqortriapp/MainActivity.java
+++ app/src/main/java/com/example/sqortriapp/MainActivity.java
@@ -6,11 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<squareNumber, eachNumber, triangularNumber> extends AppCompatActivity {
 
     public boolean showMeTheTruth(View view){
 
         EditText enteredNumber = (EditText) findViewById(R.id.enteredNumber);
+
         public boolean isTheNumberTri(){
             if(enteredNumber = triangularNumbers){
                 return true;
@@ -31,10 +32,17 @@ public class MainActivity extends AppCompatActivity {
 
     public class someNumbers{
         int enteredNumbers;
+
+        int eachNumber;
+
     }
 
     someNumbers squareNumbers = new someNumbers();
     someNumbers triangularNumbers = new someNumbers();
+
+    squareNumber = eachNumber * eachNumber;
+
+    triangularNumber = eachNumber + 1;
 
 
     @Override

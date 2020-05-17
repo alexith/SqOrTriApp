f469b95650e6f1b5c7ded905c6bc50a70d00068d
alexith <akshay.cy2@gmail.com>
2020-05-17 06:47:02 +0530

    init commit of new app called sqOrTriApp

--- app/src/main/java/com/example/sqortriapp/MainActivity.java
+++ app/src/main/java/com/example/sqortriapp/MainActivity.java
@@ -14,14 +14,25 @@ public class MainActivity<integer> extends AppCompatActivity {
         final EditText[] enteredNumber = {findViewById(R.id.enteredNumber)};
 
         class someNumbers{
+
+            //index no. for triNumberList
             private int i = 1;
+
+            //index no. for triNumberList
             private int n = 1;
+
+            /*assuming that initially the number is 1 for both list,
+             after that increment is done after loop operation*/
             private int currentTriNumber = 1;
             private int currentSqNumber = 1;
 
+            //in the class somenumbers we create two new distinc classes triangularNumbersList and squareNumberList
             private someNumbers<integer>[] triangularNumbersList = new someNumbers[0];
             private someNumbers<integer>[] squareNumberList = new someNumbers[0];
 
+
+            /*loop till index reaches 100 and
+            add all the current value related to that particular index to ArrayList*/
 
             for(i<=100){
                 currentTriNumber = currentTriNumber + i;
